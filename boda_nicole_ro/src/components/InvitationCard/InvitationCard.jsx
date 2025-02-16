c82ed89ba78c0db99015eac268bb12417dee9b6e
= <vvaleria26@gmail.com>
2025-02-16 15:02:10 -0500

    Se agregó diseño de pasaporte a la invitación

--- boda_nicole_ro/src/components/InvitationCard/InvitationCard.jsx
+++ boda_nicole_ro/src/components/InvitationCard/InvitationCard.jsx
@@ -1,18 +1,27 @@
 import 'react';
 import styles from './InvitationCard.module.css';
 import imagenNovios1Circle from '../../assets/images/novios/1.jpeg';
+import logoNovios from '../../assets/images/logo/logo-circle.png';
 
 const InvitationCard = () => {
   return (
     <section className={styles.invitationContainer}>
       <div className={styles.invitationBox}>
         <img className={styles.imageNovios1Container} src={imagenNovios1Circle} alt="Logo Novios 1" />
-        <h2 className={styles.title}>Invitación</h2>
-        <p><strong>Fecha:</strong> Septiembre 6 de 2025</p>
-        <p><strong>Hora:</strong>11AM</p>
-        <p><strong>Destino:</strong>La Quinta</p>
-        <p><strong>Iglesia:</strong>Iglesia católica del sagrado corazón - Manta</p>
-        <p><strong>Banquete:</strong> [Empresa del Banquete]</p>
+        <h2 className={styles.title}>Pasaporte a nuestra boda</h2>
+        <p className={styles.linea}></p>
+        <div className={styles.invitationContent}>
+          <div className={styles.textContent}>
+            <p><strong>Fecha:</strong><br /> Septiembre 6 de 2025</p>
+            <p><strong>Hora:</strong><br />11AM</p>
+            <p><strong>Destino:</strong><br />La Quinta</p>
+            <p><strong>Iglesia:</strong><br />Iglesia católica del sagrado corazón - Manta</p>
+            <p><strong>Banquete:</strong><br /> [Empresa del Banquete]</p>
+          </div>
+          <img className={styles.imageContainer} src={logoNovios} alt="Logo Novios" />
+
+        </div>
+        <p className={styles.linea}></p>
         <footer className={styles.footer}>Nicole & Diego</footer>
       </div>
     </section>
