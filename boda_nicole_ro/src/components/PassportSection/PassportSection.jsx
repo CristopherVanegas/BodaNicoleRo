3c0ed88a8cc04254959d9a4b6ca3224656675951
= <vvaleria26@gmail.com>
2025-02-17 23:37:36 -0500

    Se replicó el slider

--- boda_nicole_ro/src/components/PassportSection/PassportSection.jsx
+++ boda_nicole_ro/src/components/PassportSection/PassportSection.jsx
@@ -7,22 +7,27 @@ const PassportSection = () => {
   console.log(logoNovios);
   return (
     <section className={styles.passportContainer}>
-      <div className={styles.sliderBackground}>
-      </div>
-
-      <div className={styles.content}>
-        <img className={styles.imageContainer} src={logoNovios} alt="Logo Novios" />
-        <h1 className={styles.title}>Pasaporte</h1>
-        <h2 className={styles.h3}>A la boda de</h2>
-        <h2 className={styles.subtitle}>Nicole & Diego</h2>
-        <div className={styles.textContainer}>
-          <p className={styles.text1}>Después de</p>
-          <p className={styles.text2}>14 años juntos</p>
+      <div className={styles.content1}>
+        <div className={styles.sliderBackground} />
+        <div className={styles.content}>
+          <img className={styles.imageContainer} src={logoNovios} alt="Logo Novios" />
+          <h1 className={styles.title}>Pasaporte</h1>
+          <h2 className={styles.h3}>A la boda de</h2>
+          <h2 className={styles.subtitle}>Nicole & Diego</h2>
         </div>
-        <p className={styles.invitationText}>
-          prepara tus maletas y acompáñanos en este viaje
-        </p>
-        <Countdown />
+      </div>
+      <div className={styles.content2}>
+        <div className={styles.sliderBackground2} />
+        <div className={styles.content}>
+          <div className={styles.textContainer}>
+            <p className={styles.text1}>Después de</p>
+            <p className={styles.text2}>14 años juntos</p>
+          </div>
+          <p className={styles.invitationText}>
+            prepara tus maletas y acompáñanos en este viaje
+          </p>
+          <Countdown />
+        </div>
       </div>
     </section>
   );
