09bb4384624de27de6dd190a6c03e7cb222f571c
= <vvaleria26@gmail.com>
2025-02-19 15:29:06 -0500

    Se modificó donde comienza la ejecución de la animación y se eliminó .content2 porque era innecesario

--- boda_nicole_ro/src/components/Story/story.jsx
+++ boda_nicole_ro/src/components/Story/story.jsx
@@ -5,18 +5,16 @@ import Countdown from "../Countdown/Countdown";
 const Story = () => {
     return (
         <div className={styles.storyContainer}>
-            <div className={styles.content2}>
-                <div className={styles.sliderBackground2} />
-                <div className={styles.content}>
-                    <div className={styles.textContainer}>
-                        <p className={styles.text1}>Después de</p>
-                        <p className={styles.text2}>14 años juntos</p>
-                    </div>
-                    <p className={styles.invitationText}>
-                        prepara tus maletas y acompáñanos en este viaje
-                    </p>
-                    <Countdown />
+            <div className={styles.sliderBackground2} />
+            <div className={styles.content}>
+                <div className={styles.textContainer}>
+                    <p className={styles.text1}>Después de</p>
+                    <p className={styles.text2}>14 años juntos</p>
                 </div>
+                <p className={styles.invitationText}>
+                    prepara tus maletas y acompáñanos en este viaje
+                </p>
+                <Countdown />
             </div>
         </div>
     )
