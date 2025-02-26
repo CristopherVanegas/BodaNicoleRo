fa93bd832c42e0b52903b9ce6f18d1315d213b6e
Cristopher Vanegas <cristopher.platzi@gmail.com>
2025-02-26 15:19:36 -0500

    Feat: Se añadió el bg slider

--- boda_nicole_ro/src/components/Gallery/Gallery.jsx
+++ boda_nicole_ro/src/components/Gallery/Gallery.jsx
@@ -1,29 +1,44 @@
-import 'react';
-import styles from './Gallery.module.css';
-import cameraIcon from '../../assets/images/icons/camera-icon.png';
-import photo1 from '../../assets/images/novios/1.jpeg';
-import photo2 from '../../assets/images/novios/2.jpeg';
-import photo3 from '../../assets/images/novios/3.jpeg';
-import photo4 from '../../assets/images/novios/4.jpeg';
-import photo5 from '../../assets/images/novios/5.jpeg';
+import "react";
+import styles from "./Gallery.module.css";
+import cameraIcon from "../../assets/images/icons/camera-icon.png";
+import photo1 from "../../assets/images/novios/1.jpeg";
+import photo2 from "../../assets/images/novios/2.jpeg";
+import photo3 from "../../assets/images/novios/3.jpeg";
+import photo4 from "../../assets/images/novios/4.jpeg";
+import photo5 from "../../assets/images/novios/5.jpeg";
 
-import HeartCollage from '../HeartCollage/HeartCollage';
+import HeartCollage from "../HeartCollage/HeartCollage";
 
 const Gallery = () => {
-  const googleDriveLink = "https://drive.google.com/drive/folders/TU_CARPETA_ID"; // Reemplaza con tu enlace real
+  const googleDriveLink =
+    "https://drive.google.com/drive/folders/TU_CARPETA_ID"; // Reemplaza con tu enlace real
 
   return (
     <section className={styles.galleryContainer}>
-      <HeartCollage images={[photo1, photo2, photo3, photo4, photo5]} />
-      <h2 className={styles.title}>Comparte tus recuerdos</h2>
-      <p className={styles.text}>
-        El día de la boda toma muchas fotos y déjanos un bonito y divertido recuerdo haciendo click en el ícono de la cámara de fotos!
-      </p>
-      <div className={styles.buttonContainer}>
-        <div className={styles.buttonBox}>
-          <a href={googleDriveLink} target="_blank" rel="noopener noreferrer" className={styles.confirmButton} onClick={console.log("Copiado!")}>
-            <img className={styles.cameraIcon} src={cameraIcon} alt="Icono Cámara" />
-          </a>
+      <div className={styles.sliderBackground} />
+      <div className={styles.content}>
+        <HeartCollage images={[photo1, photo2, photo3, photo4, photo5]} />
+        <h2 className={styles.title}>Comparte tus recuerdos</h2>
+        <p className={styles.text}>
+          El día de la boda toma muchas fotos y déjanos un bonito y divertido
+          recuerdo haciendo click en el ícono de la cámara de fotos!
+        </p>
+        <div className={styles.buttonContainer}>
+          <div className={styles.buttonBox}>
+            <a
+              href={googleDriveLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.confirmButton}
+              onClick={console.log("Copiado!")}
+            >
+              <img
+                className={styles.cameraIcon}
+                src={cameraIcon}
+                alt="Icono Cámara"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- boda_nicole_ro/src/components/GiftSection/GiftSection.jsx
+++ boda_nicole_ro/src/components/GiftSection/GiftSection.jsx
@@ -1,27 +1,41 @@
-import 'react';
-import styles from './GiftSection.module.css';
-import bancoIcon from '../../assets/images/icons/banco-icon.png';
-import giftIcon from '../../assets/images/gift.png';
+import "react";
+import styles from "./GiftSection.module.css";
+import bancoIcon from "../../assets/images/icons/banco-icon.png";
+import giftIcon from "../../assets/images/gift.png";
 
 const GiftSection = () => {
   return (
     <section className={styles.giftContainer}>
-      <img className={styles.giftIcon} src={giftIcon} alt="Icono Regalo" />
-      <h2 className={styles.title}>Regalo en Sobre Sellado</h2>
-      <p className={styles.text}>
-        Lo más importante es su presencia, pero si deseas hacernos un regalo, aquí dejamos nuestro número de cuenta.
-      </p>
-      <div className={styles.accountsContainer}>
-        <div className={styles.accountBox}>
-          <a className={styles.confirmButton} onClick={console.log("Copiado!")}>
-            <img className={styles.bancoIcon} src={bancoIcon} alt="Icono Banco" />
-          </a>
-          <h3>Banco Pichincha</h3>
-          <p><strong>Cuenta de Ahorros:</strong> 4227865200</p>
-          <p><strong>Titular:</strong>Lorena Álava</p>
+      <div className={styles.sliderBackground} />
+      <div className={styles.content}>
+        <img className={styles.giftIcon} src={giftIcon} alt="Icono Regalo" />
+        <h2 className={styles.title}>Regalo en Sobre Sellado</h2>
+        <p className={styles.text}>
+          Lo más importante es su presencia, pero si deseas hacernos un regalo,
+          aquí dejamos nuestro número de cuenta.
+        </p>
+        <div className={styles.accountsContainer}>
+          <div className={styles.accountBox}>
+            <a
+              className={styles.confirmButton}
+              onClick={console.log("Copiado!")}
+            >
+              <img
+                className={styles.bancoIcon}
+                src={bancoIcon}
+                alt="Icono Banco"
+              />
+            </a>
+            <h3>Banco Pichincha</h3>
+            <p>
+              <strong>Cuenta de Ahorros:</strong> 4227865200
+            </p>
+            <p>
+              <strong>Titular:</strong>Lorena Álava
+            </p>
+          </div>
         </div>
       </div>
-
       <div className={styles.envelope}></div>
     </section>
   );
