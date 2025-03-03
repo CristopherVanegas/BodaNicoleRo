1c588da21a3f9b2adf44454d25385331b1dc9f23
= <vvaleria26@gmail.com>
2025-03-02 20:52:29 -0500

    Se agregaró la animación de rotateCompass como variable, esto será bueno para optimizar líneas de código. Por otro lado, se agregó un compass en la story component.

--- boda_nicole_ro/src/components/Story/story.jsx
+++ boda_nicole_ro/src/components/Story/story.jsx
@@ -1,11 +1,15 @@
 import 'react'
 import styles from "./story.module.css";
 import Countdown from "../Countdown/Countdown";
+import brujula from "../../assets/images/brujula31.png";
 
 const Story = () => {
     return (
         <div className={styles.storyContainer}>
             <div className={styles.sliderBackground2} />
+            <div className={styles.imageContainer}>
+                <img className={styles.brujula} src={brujula} alt="Brujula" />
+            </div>
             <div className={styles.content}>
                 <div className={styles.textContainer}>
                     <p className={styles.text1}>Después de</p>
